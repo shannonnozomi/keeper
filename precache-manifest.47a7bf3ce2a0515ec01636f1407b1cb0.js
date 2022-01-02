8bf2468435540f604edf29eb1c9b715600826cf3
Shannon Nicholas <shannonnozomi@gmail.com>
2022-01-02 15:43:10 -0500

    Updates

---
+++ precache-manifest.47a7bf3ce2a0515ec01636f1407b1cb0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91661f78f78d934a0186499df300a90c",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "095fb63953b4055281fd",
+    "url": "/keeper/static/js/2.d644d4a1.chunk.js"
+  },
+  {
+    "revision": "b743f676918994621a44",
+    "url": "/keeper/static/js/main.ebb6cb8d.chunk.js"
+  },
+  {
+    "revision": "af4a55ea2c5d08d39996",
+    "url": "/keeper/static/js/runtime-main.f5c12d9a.js"
+  }
+]);
